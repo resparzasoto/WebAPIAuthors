87726d837f8ef068e7a02dc1f73f383c1280ae6f
Jesús Rubén Esparza Soto <rubenesparzasoto95@gmail.com>
2022-05-15 00:35:54 -0600

    feat: beginning the web api development

---
+++ WebAPIAuthors/Controllers/AuthorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAuthors.Entities;
+
+namespace WebAPIAuthors.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Author>> Get()
+        {
+            return new List<Author>()
+            {
+                new Author() { Id = 1, Name = "Felipe" },
+                new Author() { Id = 2, Name = "Claudia" },
+            };
+        }
+    }
+}
---
+++ WebAPIAuthors/Entities/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPIAuthors.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
